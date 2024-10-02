5c4fc6a1c53d6bf964ca3ccf7d57366327304e80
Victor Nzwili <victornzwili@gmail.com>
2024-10-02 22:17:00 +0300

    added files to github

---
+++ index.js
@@ -0,0 +1,9 @@
+// Function to turn on the light..
+function turnon() {
+    document.getElementById("myimage").src = "light-bulb-on.png";
+}
+
+// Function to turn off the light..
+function turnoff() {
+    document.getElementById("myimage").src = "light-bulb-off.png";
+}
